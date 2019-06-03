5df25daeeccde8c61665c4a247977e6e4d97b360
trexgris <trexgris@gmail.com>
2019-06-03 15:17:37 +0100

    Added new func to add a json as a collection to the db

--- utest/json_extract/main.cpp
+++ utest/json_extract/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 #include <fstream>
-
+#include <string>
+#include <set>
 #include <jsoncpp/json/json.h>
-
+#include "../../include/baroudeur/mongodb/Helper.hpp"
 
 int main() {
     std::ifstream ifs("osm_response.json");
+    std::ifstream ifs2("places.json");
+    auto test = baroudeur::mongodb::Helper::Make();
     Json::Reader reader;
     Json::Value obj;
     reader.parse(ifs, obj);     // Reader can also read strings
     const Json::Value& chars = obj["features"];
-    std::set<string> unique_cities;
+    std::set<std::string> unique_cities;
 
-    for(auto && it : chars.size())
-
-    for(int i = 0 ; i < chars.size() ; ++i) {
-
-    }
+    
 
 
-std::cout <<chars.size();
+
     return 1;
 }
